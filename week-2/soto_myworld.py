c14eea777abf2e3bcd9b34b5c05b3826f23268e6
Victor Soto <135081103+FixedBits@users.noreply.github.com>
2024-01-21 04:22:18 -0700

    Exercise 2.3 – Python

---
+++ week-2/soto_myworld.py
@@ -0,0 +1,8 @@
+#Title: soto_myworld.py
+#Author: Victor Soto
+#Date: 01/21/2024
+#Description: https://www.youtube.com/watch?v=kqtD5dpn9C8
+
+# Write the code necessary to output 
+# the value “You are now in <yourLastName>’s world!”
+print("You are now in Soto's world!")
