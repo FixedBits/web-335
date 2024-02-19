3dde57c903bbeac857888a642f7d8d5fed7d64f7
Victor Soto <135081103+FixedBits@users.noreply.github.com>
2024-02-18 20:13:19 -0700

    Exercise 6.3
    – Python with MongoDB, Part 1

---
+++ week_6/soto_usersp1.py
@@ -0,0 +1,25 @@
+#---------------------------
+#Title:  soto_usersp1.py
+#Author: Victor Soto
+#Date:   02/18/2024
+#Description: Learning how to use Python to connect to MongoDB
+#---------------------------
+
+# Import the MongoClient
+from pymongo import MongoClient
+
+# Build a connection string to connect to
+client = MongoClient("mongodb+srv://web335_user:web335_user@bellevueuniversity.hxeidsl.mongodb.net/web335DB?retryWrites=true&w=majority")
+
+# Get a reference to the 'web335DB' database
+db = client.web335DB
+
+# Display all documents in the 'users' collection
+for user in db.users.find():
+    print(user)
+
+# Display a document where the employeeId is 1011
+print(db.users.find_one({"employeeId": 1011}))
+
+# Display a document where the lastName is Mozart
+print(db.users.find_one({"lastName": "Mozart"}))
